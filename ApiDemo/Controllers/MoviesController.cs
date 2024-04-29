0b23df3058b3145a7cda809af413d0dbeaef7560
Bernat Pujol <bernat.pujol@keysight.com>
2024-04-29 14:18:09 +0200

    Completed GET {id} request, where it returns the movie that contains that ID, otherwise, returns error with Status=OK to let the caller know the API is not down

--- ApiDemo/Controllers/MoviesController.cs
+++ ApiDemo/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiDemo.Models;
+using ApiDemo.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiDemo.Controllers
@@ -7,5 +9,51 @@ namespace ApiDemo.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+
+        private readonly ModelService modelService;
+
+        public MoviesController(ModelService _modelService) 
+        {
+            modelService = _modelService;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/MoviesController
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Movie movie = new Movie();
+            if (modelService.GetMovie(id, ref movie) == true)
+            {
+                return this.Ok(new { Movie = movie, Status = "OK" });
+            }
+            else
+            {
+                return NotFound(new { Id = id, Message = "Movie not found", Status = "OK" });
+            }
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MoviesController>/
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MoviesController>/
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
