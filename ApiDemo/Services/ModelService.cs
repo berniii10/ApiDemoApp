e85723bebde12d3a425353c298baf8a262bd8a58
Bernat Pujol <bernat.pujol@keysight.com>
2024-04-29 11:36:20 +0200

    Created a new Service and controller for the Model

---
+++ ApiDemo/Services/ModelService.cs
@@ -0,0 +1,12 @@
+﻿using ApiDemo.Models;
+
+namespace ApiDemo.Services
+{
+    public class ModelService
+    {
+        Model model { get; set; }
+        public ModelService(Model _model) {
+            model = _model;
+        }
+    }
+}
